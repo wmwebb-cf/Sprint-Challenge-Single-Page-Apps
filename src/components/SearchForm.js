e1fb7e92b14f51404cece932d48eb0e7d60b51df
William Webb <webbdevvue@gmail.com>
2020-03-20 21:46:27 -0400

    stuff

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 
 
 export default function SearchForm() {
 
   const [searchTerm, setSearchTerm] = useState('');
-    const handleChange = event => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [data, setData] = useState([]);
+  const handleChange = event => {
       setSearchTerm(event.target.value);
   };
+  useEffect(() => {
+    axios.get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/').then(res => {
+      setData(res.data.results);
+    })
+
+    const results = data.filter(item =>
+      console.log(item)
+      // item.includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
  
   return (
     <section className="search-form">
